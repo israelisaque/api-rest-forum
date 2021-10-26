d0f28230334ec33b9bf17386020e94dd60ec5981
Israel Oliveira <israelisaque@hotmail.com>
2021-10-26 20:34:33 -0300

    Adicionando busca de tópicos por nome do curso

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -4,6 +4,13 @@ import br.com.alura.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    List<Topico> findByCurso_Nome(String nomeCurso);
+
+//    @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+//    List<Topico> carregarProNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 }
