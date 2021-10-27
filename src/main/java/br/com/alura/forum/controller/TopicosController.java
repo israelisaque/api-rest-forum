4d56abcee44f43f3221d88d9caf40901ea3b9628
Israel Oliveira <israelisaque@hotmail.com>
2021-10-27 19:57:33 -0300

    Implementação do cadastro de topicos e CursoRepository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,26 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.form.TopicoFormDTO;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
     public List<TopicoDTO> lista(String nomeCurso) {
         if (nomeCurso == null) {
             List<Topico> topicos = topicoRepository.findAll();
@@ -24,5 +29,11 @@ public class TopicosController {
             List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
             return TopicoDTO.converter(topicos);
         }
+    }
+
+    @PostMapping
+    public void cadastrar(@RequestBody TopicoFormDTO topicoFormDTO) {
+        Topico topico = topicoFormDTO.converter(cursoRepository);
+        topicoRepository.save(topico);
     }
 }
---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoFormDTO.java
@@ -0,0 +1,22 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TopicoFormDTO {
+
+    private String titulo;
+    private String mensagem;
+    private String nomeCurso;
+
+    public Topico converter(CursoRepository cursoRepository) {
+        Curso curso = cursoRepository.findByNome(nomeCurso);
+        return new Topico(titulo, mensagem, curso);
+    }
+}
