6b13d6a550fa6c4f5e61dadef114d11fd515f472
Israel Oliveira <israelisaque@hotmail.com>
2021-11-21 11:40:21 -0300

    Implementado UsuarioRepository e a classe AutenticacaoService que implementa UserDetailsService

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$4v7u5sq962sl0N2nlqeFrOj4b6P4gTc6NJMd4dJp.ylvV2T5CU6H2');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
